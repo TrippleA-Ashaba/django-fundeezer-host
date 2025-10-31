bcf8dee02cb2e736467baf53a5e5d627ff765331
Ashaba Aheebwa Albert <102288573+TrippleA-Ashaba@users.noreply.github.com>
2025-10-31 21:35:00 +0300

    csrf

--- fundezer_host/settings.py
+++ fundezer_host/settings.py
@@ -120,3 +120,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CSRF_TRUSTED_ORIGINS = ["https://fundezer-fundezer-l8gi2m-b75c0f-157-173-123-193.traefik.me"]
